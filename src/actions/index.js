b423abbe0cd6967ca9e6a67cfd0bdbe330ce256c
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-06 18:57:28 -0800

    Created REST action creators to create, fetch, modify, and delete streams.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
 import streams from "../apis/stream";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
+import stream from "../apis/stream";
 
 export const signIn = userId => {
   return {
@@ -18,4 +27,28 @@ export const createStream = formValues => async dispatch => {
   const response = await streams.post("/streams", formValues);
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await stream.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await stream.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await stream.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await stream.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
