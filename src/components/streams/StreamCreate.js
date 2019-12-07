a07e05da5193e16e394fe9073d52cc1b5caec3e3
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-06 18:33:01 -0800

    Wired up Redux Thunk in index.js. Implemented connect function in StreamCreate to utilize createStream action creator (POST request).

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,8 +25,9 @@ class StreamCreate extends React.Component {
     );
   };
 
+  // Will attempt to create new stream if our form is valid
   onSubmit(formValues) {
-    console.log(formValues);
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -59,7 +62,10 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+// For easy syntax handling with connect function
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
