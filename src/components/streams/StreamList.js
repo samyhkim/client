66b07fa8b37c1a08d1f671a2b89aab919693570f
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-07 15:02:13 -0800

    Created renderAdmin helper function for user to edit or delete their streams.

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,10 +7,24 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  // Helper function to compare the user ID of the stream that we are iterating over with this.props.userId
+  // Allows current user to execute administrative actions such as edit and delete their own streams
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -20,6 +34,7 @@ class StreamList extends React.Component {
       );
     });
   }
+
   render() {
     return (
       <div>
@@ -32,7 +47,11 @@ class StreamList extends React.Component {
 
 const mapStateToProps = state => {
   // Takes object as an argument and inserts all the different values into an array
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    // Allows our component to know the ID of the user who is currently signed into the application
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
