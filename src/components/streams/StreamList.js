902c72bfcfadfdcd3599cb63e7b4c5ee542c0ef1
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-07 15:21:59 -0800

    Created renderCreate helper function and added more comments in StreamList component.

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
+  // Grabs all streams from API server upon component initialization
   componentDidMount() {
     this.props.fetchStreams();
   }
@@ -20,6 +22,7 @@ class StreamList extends React.Component {
     }
   }
 
+  // Renders list of streams in state
   renderList() {
     return this.props.streams.map(stream => {
       return (
@@ -35,23 +38,40 @@ class StreamList extends React.Component {
     });
   }
 
+  // Renders 'Create Stream' button if user is signed in
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Streaem
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
+    console.log(this.props.streams);
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  // Takes object as an argument and inserts all the different values into an array
   return {
+    // Takes object as an argument and inserts all the different values into an array
     streams: Object.values(state.streams),
     // Allows our component to know the ID of the user who is currently signed into the application
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   };
 };
 
+// Allows component to use fetchStreams action creator
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
