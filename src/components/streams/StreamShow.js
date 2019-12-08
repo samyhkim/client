ac1646ef7dac3aff5fc3e3af81a927d168872da2
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-07 20:11:58 -0800

    Finished building StreamShow component, which connects to local RTMP server and utilizes component lifecycle methods.

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,45 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Use ref system to get access to elemeent in DOM
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  // In case we do not have access to our stream and we have to fetch it on the fly
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  // Handles flv player to stop attempting to download stream/video when component is unmounted
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    // Prevent function if we already have a player or if we do not have a stream yet
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -15,6 +50,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
