818267330633a0be3718bfdf4ff2865456b0325f
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-07 17:10:35 -0800

    Started StreamCreate and StreamEdit refactor to utilize StreamForm as their templates.

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { fetchStream, editStream } from "../../actions";
 
-const StreamEdit = props => {
-  console.log(props);
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit(formValues) {
+    this.props.editStream(this.props.match.params.id, formValues);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field></Field>
+        <Field></Field>
+        <button></button>
+      </form>
+    );
+  }
+}
+
+// ownProps gives us access to StreamEdit's default props passed down from Router
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
